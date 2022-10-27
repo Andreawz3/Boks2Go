a0287b1390a0e7ec1471413c2845e0908b3690b9
Andrea Wu <andrea.wzg@gmail.com>
2022-10-26 18:53:26 -0700

    add modal box for card info in shopping web page

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,24 @@
-// window.onload = () => {
+window.onload = () => {
 
-// };
+};
 
 function search() {
     const input = document.getElementsByClassName('input');
     input[0].style.width = '200px';
 }
-
 function unselect() {
     const input = document.getElementsByClassName('input');
     if (input[0].style.width == '200px' && input[0].value.trim() == '') {
         input[0].style.width = '75px'
+    }
+}
+function modalBox() {
+    const cardInfo = document.getElementById('modal-box');
+    cardInfo.style.display = 'block';
+}
+function closeButton() {
+    const cardInfo = document.getElementById('modal-box');
+    if (cardInfo.style.display === 'block') {
+        cardInfo.style.display = 'none';
     }
 }
