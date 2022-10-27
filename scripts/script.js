8066c15a1727fa3b3131364fd9090272b611141f
Andrea Wu <andrea.wzg@gmail.com>
2022-10-26 23:12:45 -0700

    add image and info on menu page & add animation to 'About Chef' and 'Chef Recomendation' on main page

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,30 @@
-window.onload = () => {
+window.onscroll = (e) => {
+    if (window.scrollY > 160) {
+        const aboutChef = document.getElementById('chef-info');
+        aboutChef.classList.add('slidingInfo');
 
-};
-
+        const chefImg = document.getElementById('chef-image');
+        chefImg.classList.add('slidingImg')
+    }
+    if (window.scrollY > 1300) {
+        const chefRecom = document.getElementById('dishes-info');
+        chefRecom.classList.add('slidingChefRec');
+    }
+}
 function search() {
-    const input = document.getElementsByClassName('input');
-    input[0].style.width = '200px';
+    const input = document.getElementById('input');
+    input.style.width = '200px';
 }
 function unselect() {
-    const input = document.getElementsByClassName('input');
-    if (input[0].style.width == '200px' && input[0].value.trim() == '') {
-        input[0].style.width = '75px'
+    const input = document.getElementById('input');
+    if (input.style.width == '200px' && input.value.trim() == '') {
+        input.style.width = '75px'
     }
 }
 function modalBox() {
     const cardInfo = document.getElementById('modal-box');
     cardInfo.style.display = 'block';
+    
 }
 function closeButton() {
     const cardInfo = document.getElementById('modal-box');
